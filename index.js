08bb54a29f70041069e6cb07fe562411986da48d
thejas-1999 <thejask074@gmail.com>
2024-03-23 12:07:16 +0530

    create an api for update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/api/products", async (req, res) => {
 });
 
 //recieve individual product
-app.get("/api/products/:id", async (req, res) => {
+app.get("/api/product/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const product = await Product.findById(id);
@@ -41,6 +41,22 @@ app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update a product
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).json({ message: `Product not found` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
