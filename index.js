b8f30571f3363e07a8b430b2defa323722abdad3
thejas-1999 <thejask074@gmail.com>
2024-03-23 11:25:42 +0530

    add some products in to the database

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(`Hello world iam node server updated`);
+  res.send(`Hello world, I am a node server.`);
 });
 
-//recieve data
-app.post("/api/products", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+// Receive data
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
   .connect(
-    "mongodb+srv://ThejasK:Thejas1999@cluster0.iczrrlg.mongodb.net/Node-API?retryWrites=true&w=majority"
+    "mongodb+srv://ThejasK:Thejas1999@cluster0.iczrrlg.mongodb.net/Node-API?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
     console.log(`Connected to database`);
     app.listen(3000, () => {
-      console.log(`app running on http://localhost:3000`);
+      console.log(`App running on http://localhost:3000`);
     });
   })
-  .catch(() => console.log(`Connection failed`));
+  .catch((error) => {
+    console.error("Connection failed:", error.message);
+    process.exit(1); // Exit the process if connection fails
+  });
