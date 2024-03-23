6594c8394e89e8437d7219901b23e95d0c22d581
thejas-1999 <thejask074@gmail.com>
2024-03-23 10:57:25 +0530

    create a database model and connect to index.js

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send(`Hello world iam node server updated`);
+});
+
+//recieve data
+app.post("/api/products", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 mongoose
