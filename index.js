44f787d9bc91e0193e30f1b0e7301f9db0774f35
thejas-1999 <thejask074@gmail.com>
2024-03-23 11:48:48 +0530

    create a api for viewing individual products

--- index.js
+++ index.js
@@ -9,7 +9,34 @@ app.get("/", (req, res) => {
   res.send(`Hello world, I am a node server.`);
 });
 
-// Receive data
+//view data
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.send(500).json({ message: error.message });
+  }
+});
+
+//recieve individual product
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      // If product with specified ID is not found, send 404 status
+      return res.status(404).json({ message: "Product not found" });
+    }
+    // Send 200 status along with the product if found
+    res.status(200).json(product);
+  } catch (error) {
+    // Send 500 status in case of server error
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// add data
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -21,11 +48,7 @@ app.post("/api/products", async (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://ThejasK:Thejas1999@cluster0.iczrrlg.mongodb.net/Node-API?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
+    "mongodb+srv://ThejasK:Thejas1999@cluster0.iczrrlg.mongodb.net/Node-API?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log(`Connected to database`);
